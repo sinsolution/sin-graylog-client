e9fd9a7e4c9863fb096b81e84552598d7f104d23
Hillus Hilbert <hillus.hilbert@codding.com.br>
2022-04-13 20:39:46 -0300

    fix: corrindo config mysql demais parametros default

--- config/graylog.php
+++ config/graylog.php
@@ -4,9 +4,15 @@ return [
     'host'   => env('GRAYLOG_HOST', '127.0.0.1'),
     'port'   => env('GRAYLOG_PORT', 4718),
     'mysql' => [
+        'driver' => 'mysql',
         "host" => env('SINLOG_MYSQL_HOST','localhost'),
         "database" => env('SINLOG_MYSQL_DATABASE','applog'),
         "username" => env('SINLOG_MYSQL_USER','root'),
-        "password" => env('SINLOG_MYSQL_PASSWORD','root')
+        "password" => env('SINLOG_MYSQL_PASSWORD','root'),
+        "port" => env('SINLOG_MYSQL_PORT','3306'),
+        "charset" => env('SINLOG_MYSQL_CHARSET','utf8'),
+        "collation" => env('SINLOG_MYSQL_COLLATION','utf8_unicode_ci'),
+        'prefix'    => '',
+        'strict'    => false,
     ],
 ];
